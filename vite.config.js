13f8eb66d79fc5a1e500d5448b3ab98c15b8d16c
Francis Wang <franciswang@NJKNHVVGV9.local>
2024-09-24 17:15:12 -0400

    add function to remove source references

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Plugin to remove sourceMappingURL from the output files
+function removeSourceMapPlugin() {
+  return {
+    name: 'remove-sourcemaps',
+    enforce: 'post',
+    apply: 'build',
+    transformIndexHtml(html) {
+      return html.replace(/\/\/# sourceMappingURL=.*\.map/g, '')
+    },
+    generateBundle(_, bundle) {
+      for (const file in bundle) {
+        if (bundle[file].type === 'asset' && bundle[file].source) {
+          bundle[file].source = bundle[file].source.toString().replace(/\/\/# sourceMappingURL=.*\.map/g, '')
+        }
+      }
+    }
+  }
+}
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeSourceMapPlugin()],
+  build: {
+    sourcemap: false
+  }
 })
